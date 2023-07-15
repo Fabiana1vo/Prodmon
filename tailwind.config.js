74f887145418aabe3eb64da24fd72e1ee7edcc99
Fabiana1vo <fabianasouzaivo@icloud.com>
2023-07-15 20:05:36 -0300

    ajustei o hover dos botões

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+      'aqua-500':'#48D6D2',
+      'gray-invisible': '#d9d9d955'
+      },
+    },
   },
   plugins: [],
 }
